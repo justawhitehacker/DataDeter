a1cf601c1faf704e457a91537ca839957254b447
justawhitehacker <75467556+justawhitehacker@users.noreply.github.com>
2025-09-01 11:41:02 +0700

    Update example.projectsimple.lua

--- example.projectsimple.lua
+++ example.projectsimple.lua
@@ -1,4 +1,6 @@
 -- RaihanMan18
+
+-- i made this example, you can copy this example code and custom your own
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 local Players = game:GetService("Players")
@@ -6,7 +8,7 @@ local Players = game:GetService("Players")
 local mods = ServerStorage:WaitForChild("Modules")
 local DataDeter = require(mods.DataDeter)
 
-DataDeter.SetServerSecret("MyHandsAreFullOfSinsButCouldIForgiveMyselfToGodForMyBadBehavioursInThisWorld?", 75) -- global security_level to 75
+DataDeter.SetServerSecret("TOP-SECRET", 75) -- global security_level to 75, set the server secret
 
 local plrData = DataDeter.InDataInfo("PlayersData", "global", {
     security_level = 75,
@@ -29,12 +31,69 @@ Players.PlayerAdded:Connect(function(plr)
     local ok, datas = pcall(function()
         return playerData:Get(plr)
     end)
-    if not ok then
+    if not ok or typeof(datas) ~= "table" then
         warn("data cannot be obtained")
         datas = {}
     end
     datas = datas or {}
 
+    local bindOK, errBind = playerData:BindData(plr)
+    if not bindOK then
+        warn("data cannot be binded due to: " .. tostring(errBind))
+    end
+
+    local objectsCount = tonumber(datas.obj_count) or 0
+    local objs = datas.objects or {}
+
+    local baseModel = Instance.new("Model")
+    baseModel.Name = plr.Name .. "_base"
+    baseModel.Parent = workspace.WorldBlock
+        
+    -- deserialize objects
+    task.spawn(function()
+        local countBatch = 0
+        for i = 1, objectsCount do
+            local obj = objs[i]
+
+            local pos = Vector3.new(obj.pos and obj.pos.x or 0,    obj.pos and obj.pos.y or 0,    obj.pos and obj.pos.z or 0)
+            local rVec = Vector3.new(obj.rightvec and obj.rightvec.x or 1,    obj.rightvec and obj.rightvec.y or 0,    obj.rightvec and obj.rightvec.z or 0)
+            local uVec = Vector3.new(obj.upvec and obj.upvec.x or 0,    obj.upvec and obj.upvec.y or 1,    obj.upvec and obj.upvec.z or 0)
+                    
+            local part = Instance.new("Part")
+            part.Name = obj.name
+            part.CFrame = CFrame.fromMatrix(pos, rVec, uVec)
+            part.Anchored = true
+            part.CanCollide = true
+            if obj.color and typeof(obj.color) == "table" then
+                local color = obj.color
+                part.Color = Color3.fromRGB(color.r, color.g, color.b)
+            end
+            part.Size = Vector3.new(obj.size and obj.size.x or 1, obj.size and obj.size.y or 1, obj.size and obj.size.z or 1)
+            part.Parent = baseModel
+
+            countBatch = countBatch + 1
+            if countBatch >= 30 then
+                task.wait()
+                countBatch = 0 -- reset counter
+            end
+        end
+
+        local previousPlace = datas.previous_place_cframe
+        if not previousPlace or not previousPlace.right_vec or not previousPlace.up_vec then return end
+
+        local pos = Vector3.new(previousPlace.x,    previousPlace.y,    previousPlace.z)        
+        
+        local rightVec = Vector3.new(previousPlace.right_vec.x,    previousPlace.right_vec.y,    previousPlace.right_vec.z)
+        local upVec = Vector3.new(previousPlace.up_vec.x,    previousPlace.up_vec.y,    previousPlace.up_vec.z)
+
+        -- roblox automatically calculating for cross product, but i did manual instead
+        local lookVec = rightVec:Cross(upVec).Unit -- i used cross product then normalize vector manually, you can skip this line
+                
+        -- base translations
+        baseModel:PivotTo(CFrame.fromMatrix(pos, rightVec, upVec))
+    end)
+
+    -- saving the loaded base, to prevent data loss
     local lockSuccess, ownerId = playerData:AcquireSessionLock(user, 5)
     if lockSuccess then
         local ok, err = pcall(function()
@@ -47,47 +106,7 @@ Players.PlayerAdded:Connect(function(plr)
         warn("data session cannot be locked")
     end       
 
-    local objectsCount = datas.data_counts
-
-    local baseModel = Instance.new("Model")
-    baseModel.Name = plr.Name .. "_base"
-    baseModel.Parent = workspace.WorldBlock
-        
-    -- deserialize objects
-    task.spawn(function()
-        for i = 1, objectsCount do
-            local obj = datas.objects[i]
-
-            local pos = Vector3.new(obj.pos.x,    obj.pos.y,    obj.pos.z)
-            local rVec = Vector3.new(obj.rightvec.x,    obj.rightvec.y,    obj.rightvec.z)
-            local uVec = Vector3.new(obj.upvec.x,    obj.upvec.y,    obj.upvec.z)
-                    
-            local part = Instance.new("Part")
-            part.Name = obj.name
-            part.CFrame = CFrame.fromMatrix(pos, rVec, uVec)
-            part.Anchored = true
-            part.CanCollide = true
-            part.Color = Color3.fromRGB(obj.color.r, obj.color.g, obj.color.b)
-            part.Size = Vector3.new(obj.size.x, obj.size.y, obj.size.z)
-            part.Parent = baseModel
-        end
-
-        local previousPlace = datas.previous_place_cframe
-
-        local pos = Vector3.new(previousPlace.x,    previousPlace.y,    previousPlace.z)        
-        
-        local rightVec = Vector3.new(previousPlace.right_vec.x,    previousPlace.right_vec.y,    previousPlace.right_vec.z)
-        local upVec = Vector3.new(previousPlace.up_vec.x,    previousPlace.up_vec.y,    previousPlace.up_vec.z)
-
-        -- roblox automatically calculating for cross product, but i did manual instead
-        local lookVec = rightVec:Cross(upVec).Unit -- i used cross product then normalize vector manually, you can skip this line
-                
-        -- base translations
-        baseModel:PivotTo(CFrame.fromMatrix(pos, rightVec, upVec, lookVec))
-    end)
-
     -- at best counter
-    playerData:BindData(plr)
     playerData:SmartCleanCache(15)
 end)
 
